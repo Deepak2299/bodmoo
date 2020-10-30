c8cdbf910e9988d85864a34748b432bdbe695b77
Deepak2299 <deep.agg2299@gmail.com>
2020-10-30 14:07:21 +0530

    solve shimmer problem

--- lib/providers/ScreenProvider.dart
+++ lib/providers/ScreenProvider.dart
@@ -5,28 +5,33 @@ class ScreenProvider extends ChangeNotifier {
 //  ScreenProvider(value) : super(value);
   ScreenData screenData = new ScreenData();
   bool click = false;
+  int i = 0;
   ScreenData get getScreenData => screenData;
-//  String get catgName =>screenData.catgName;
-  void add(String name, int i) {
+  int get pos => i;
+  void add(String name, int im) {
     print(name);
 //    remove();
 //    notifyListeners();
 //    if (click)
-    switch (i) {
+    switch (im) {
       case 0:
         screenData.catgName = name;
         screenData.subCatgName = null;
         screenData.brandName = null;
+        i = 1;
         break;
       case 1:
         screenData.subCatgName = name;
         screenData.brandName = null;
+        i = 2;
         break;
       case 2:
         screenData.brandName = name;
+        i = 3;
         break;
       case 3:
         screenData.vehicleName = name;
+        i == -1;
         break;
       default:
         break;
--- lib/widgets/items_view.dart
+++ lib/widgets/items_view.dart
@@ -21,6 +21,144 @@ class ItemView extends StatefulWidget {
 
 class _ItemViewState extends State<ItemView> {
   bool collapse = true;
+
+  Widget show(context, snapshots) {
+    return collapse
+        ? SizedBox(
+            height: 100,
+            child: ListView.separated(
+//                            shrinkWrap: true,
+//                            padding: EdgeInsets.all(8),
+
+              itemBuilder: (context, i) {
+                return GestureDetector(
+                  onTap: () => widget.context
+                      .read<ScreenProvider>()
+                      .add(snapshots.data[i], widget.i),
+                  child: Container(
+//                              height: 200,
+                    width: 100,
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                        // ignore: missing_return
+                        image: AssetImage(IMAGE),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black38, BlendMode.hardLight),
+                      ),
+                    ),
+                    padding: EdgeInsetsDirectional.only(top: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          snapshots.data[i].toString(),
+//                    style: textStyle,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+
+              itemCount: snapshots.data.length,
+              scrollDirection: Axis.horizontal,
+              separatorBuilder: (context, i) => SizedBox(
+                width: 10,
+              ),
+            ),
+          )
+        : GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                childAspectRatio: 1),
+            itemCount: snapshots.data.length,
+            itemBuilder: (context, i) {
+              return GestureDetector(
+                onTap: () => widget.context
+                    .read<ScreenProvider>()
+                    .add(snapshots.data[i], widget.i),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      // ignore: missing_return
+                      image: AssetImage(IMAGE),
+                      fit: BoxFit.cover,
+                      colorFilter:
+                          ColorFilter.mode(Colors.black38, BlendMode.hardLight),
+                    ),
+                  ),
+                  padding: EdgeInsetsDirectional.only(top: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        snapshots.data[i].toString(),
+//                    style: textStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            });
+  }
+
+  Widget loading(context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300],
+      highlightColor: Colors.grey[100],
+      child: collapse
+          ? SizedBox(
+              height: 100,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (context, i) => SizedBox(
+                  width: 10,
+                ),
+                shrinkWrap: true,
+                itemBuilder: (_, __) => Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white),
+                      padding: EdgeInsetsDirectional.only(top: 15),
+                    )),
+                itemCount: 6,
+              ))
+          : GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  childAspectRatio: 1),
+              itemCount: 6,
+              itemBuilder: (context, i) {
+                return Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white),
+                  padding: EdgeInsetsDirectional.only(top: 15),
+                );
+              }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Provider.value(
@@ -55,152 +193,26 @@ class _ItemViewState extends State<ItemView> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FutureBuilder(
-              future: widget.futureFunction,
-              builder: (context, snapshots) {
-                if (snapshots.hasData) {
-                  return collapse
-                      ? SizedBox(
-                          height: 100,
-                          child: ListView.separated(
-//                            shrinkWrap: true,
-//                            padding: EdgeInsets.all(8),
-
-                            itemBuilder: (context, i) {
-                              return GestureDetector(
-                                onTap: () => widget.context
-                                    .read<ScreenProvider>()
-                                    .add(snapshots.data[i], widget.i),
-                                child: Container(
-//                              height: 200,
-                                  width: 100,
-
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    image: DecorationImage(
-                                      // ignore: missing_return
-                                      image: AssetImage(IMAGE),
-                                      fit: BoxFit.cover,
-                                      colorFilter: ColorFilter.mode(
-                                          Colors.black38, BlendMode.hardLight),
-                                    ),
-                                  ),
-                                  padding: EdgeInsetsDirectional.only(top: 15),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      Text(
-                                        snapshots.data[i].toString(),
-//                    style: textStyle,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            },
-
-                            itemCount: snapshots.data.length,
-                            scrollDirection: Axis.horizontal,
-                            separatorBuilder: (context, i) => SizedBox(
-                              width: 10,
-                            ),
-                          ),
-                        )
-                      : GridView.builder(
-                          shrinkWrap: true,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 3,
-                                  crossAxisSpacing: 15,
-                                  mainAxisSpacing: 15,
-                                  childAspectRatio: 1),
-                          itemCount: snapshots.data.length,
-                          itemBuilder: (context, i) {
-                            return GestureDetector(
-                              onTap: () => widget.context
-                                  .read<ScreenProvider>()
-                                  .add(snapshots.data[i], widget.i),
-                              child: Container(
-                                height: 200,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    // ignore: missing_return
-                                    image: AssetImage(IMAGE),
-                                    fit: BoxFit.cover,
-                                    colorFilter: ColorFilter.mode(
-                                        Colors.black38, BlendMode.hardLight),
-                                  ),
-                                ),
-                                padding: EdgeInsetsDirectional.only(top: 15),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    Text(
-                                      snapshots.data[i].toString(),
-//                    style: textStyle,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          });
-                } else {
-                  return Shimmer.fromColors(
-                    baseColor: Colors.grey[300],
-                    highlightColor: Colors.grey[100],
-                    child: collapse
-                        ? SizedBox(
-                            height: 100,
-                            child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              separatorBuilder: (context, i) => SizedBox(
-                                width: 10,
-                              ),
-                              shrinkWrap: true,
-                              itemBuilder: (_, __) => Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0),
-                                  child: Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(15),
-                                        color: Colors.white),
-                                    padding:
-                                        EdgeInsetsDirectional.only(top: 15),
-                                  )),
-                              itemCount: 6,
-                            ))
-                        : GridView.builder(
-                            shrinkWrap: true,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3,
-                                    crossAxisSpacing: 15,
-                                    mainAxisSpacing: 15,
-                                    childAspectRatio: 1),
-                            itemCount: 6,
-                            itemBuilder: (context, i) {
-                              return Container(
-                                height: 200,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    color: Colors.white),
-                                padding: EdgeInsetsDirectional.only(top: 15),
-                              );
-                            }),
-                  );
-                }
-              },
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: FutureBuilder(
+                future: widget.futureFunction,
+                builder: (context, snapshots) {
+                  switch (snapshots.connectionState) {
+                    case ConnectionState.waiting:
+                      if (widget.i == context.watch<ScreenProvider>().pos)
+                        return loading(context);
+                      else {
+                        return show(context, snapshots);
+                      }
+                      break;
+                    default:
+                      if (snapshots.hasError)
+                        return loading(context);
+                      else
+                        return show(context, snapshots);
+                  }
+                },
+              )),
         ],
       ),
     );
