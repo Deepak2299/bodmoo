604b52350621cbcb3a7f5a87d7fcb720b752a232
Prayant <prayantgupta15@gmail.com>
2020-11-01 00:03:44 +0530

    Order Models

--- lib/models/cartModel.dart
+++ lib/models/cartModel.dart
@@ -1,4 +1,41 @@
-class CartModel {
-  String brandName;
+class orderModel
+{
+
+  String orderId;
+  String orderStatus;
+  String orderPlaceDate;
+  String orderDeliverDate;
+  int numberOfItemsOrdered;
+}
+
+class orderDetailsModel
+{
+  int cartTotal;
+  int totalItems;
+  List<orderItemModel>parts;
+}
+
+class orderItemModel
+{
+  String brandName, vehicleName, vehicleModel,modelYear;
+  int qty;
+  int price;
+  String partName;
   String partId;
+}
+
+
+class placeOrderModel
+{
+  List<orderItemModel> itemsOrdered;
+  String transactionId;
+  UserModel userModel;
+}
+
+class UserModel
+{
+  String userId;
+  String customerName;
+  String deliveryAdd;
+  String phoneNumber;
 }
