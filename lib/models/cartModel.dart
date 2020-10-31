888f92a75d17b18800c0ebdcce8fe4a64617b97a
Prayant <prayantgupta15@gmail.com>
2020-11-01 00:29:01 +0530

    Order Models

--- lib/models/cartModel.dart
+++ lib/models/cartModel.dart
@@ -1,5 +1,4 @@
-class getOrdersModel
-{
+class getOrdersModel {
   String orderId;
   String orderStatus;
   String orderPlaceDate;
@@ -28,4 +27,23 @@ class UserModel {
   String customerName;
   String deliveryAdd;
   String phoneNumber;
+}
+
+
+
+//POST ORDER
+{
+  "user_id":"dada",
+  "customer_name":"RAM",
+  "Delivery_add":'A-100, sec37 fbd',
+  'phone_numer':"584223233",
+  'transaction_id':"razorpay_id1333",
+  'orderdate':2020-10-21,
+  'items':
+  [
+    {
+      'brand_name': 'dad',
+      
+    }
+  ]
 }
