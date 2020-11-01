01d2bd4114e8fc9d3621d853fcc938cca5d62e16
Deepak2299 <deep.agg2299@gmail.com>
2020-11-01 23:40:23 +0530

    minor change

--- lib/PartDetailsScreen.dart
+++ lib/PartDetailsScreen.dart
@@ -40,7 +40,9 @@ class _PartDetailsScrenState extends State<PartDetailsScren> {
             children: [
               Container(
                 height: MediaQuery.of(context).size.height * 0.5,
-                child: Hero(tag: "images_${widget.partIndex}", child: Image.asset(IMAGE)),
+                child: Hero(
+                    tag: "images_${widget.partIndex}",
+                    child: Image.asset(IMAGE)),
               ),
               Text(
                 widget.partModel.details[widget.partIndex].partName.toString(),
@@ -52,15 +54,22 @@ class _PartDetailsScrenState extends State<PartDetailsScren> {
                 child: FlatButton(
                   padding: EdgeInsets.all(0),
                   // height: 20,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                  color:
-                      widget.partModel.details[widget.partIndex].outOfStock ? Colors.red.shade50 : Colors.green.shade50,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  color: widget.partModel.details[widget.partIndex].outOfStock
+                      ? Colors.red.shade50
+                      : Colors.green.shade50,
                   onPressed: () {},
                   child: Text(
-                    widget.partModel.details[widget.partIndex].outOfStock ? "OutOfStock" : "Instock",
+                    widget.partModel.details[widget.partIndex].outOfStock
+                        ? "OutOfStock"
+                        : "Instock",
 //                textAlign: TextAlign.,
                     style: TextStyle(
-                      color: widget.partModel.details[widget.partIndex].outOfStock ? Colors.red : Colors.green,
+                      color:
+                          widget.partModel.details[widget.partIndex].outOfStock
+                              ? Colors.red
+                              : Colors.green,
                     ),
                   ),
                 ),
@@ -69,7 +78,9 @@ class _PartDetailsScrenState extends State<PartDetailsScren> {
 //            height: 10,
 //          ),
               Text(
-                "Rs " + widget.partModel.details[widget.partIndex].itemPrice.toString(),
+                "Rs " +
+                    widget.partModel.details[widget.partIndex].itemPrice
+                        .toString(),
                 style: TextStyle(fontSize: 30),
               ),
             ],
@@ -98,15 +109,25 @@ class _PartDetailsScrenState extends State<PartDetailsScren> {
                         color: Colors.white,
                         onPressed: () {
                           OrderItemModel item = new OrderItemModel(
-                              partId: widget.partModel.details[widget.partIndex].id,
+                              partId:
+                                  widget.partModel.details[widget.partIndex].id,
                               brandName: widget.partModel.carBrand,
                               vehicleName: widget.partModel.carName,
                               vehicleModel: widget.partModel.carModel,
-                              vehicleYear: widget.partModel.modelYear.toString(),
-                              partName: widget.partModel.details[widget.partIndex].partName,
-                              partPrice: widget.partModel.details[widget.partIndex].itemPrice.toString(),
+                              vehicleYear:
+                                  widget.partModel.modelYear.toString(),
+                              partName: widget
+                                  .partModel.details[widget.partIndex].partName,
+                              partPrice: widget
+                                  .partModel.details[widget.partIndex].itemPrice
+                                  .toString(),
                               orderQty: 1);
-                          Provider.of<ScreenProvider>(context, listen: false).itemAdd(item);
+                          Provider.of<ScreenProvider>(context, listen: false)
+                              .itemAdd(item);
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => CartItemsScreen()));
                         },
                         child: Center(
                           child: Text(
@@ -124,7 +145,10 @@ class _PartDetailsScrenState extends State<PartDetailsScren> {
                       child: RaisedButton(
                         color: Colors.white,
                         onPressed: () {
-                          Navigator.push(context, CupertinoPageRoute(builder: (context) => CartItemsScreen()));
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => CartItemsScreen()));
                         },
                         child: Center(
                           child: Text(
