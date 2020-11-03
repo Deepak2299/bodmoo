5b0544d95bc393f2ab399c8356de9224b5106cbe
Prayant <prayantgupta15@gmail.com>
2020-11-03 23:36:57 +0530

    Firebase Intg

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -5,31 +5,25 @@ String IMAGE = 'assets/bike.png';
 
 //CATEGORY URL
 String GET_CATG_URL = BASE_URL + 'store/products/categories/';
-String ADD_CATG_URL = BASE_URL + 'store/products/addCategory/';
 
 //SUB CATG
-String GET_SUB_CATG_URL = BASE_URL + 'getByCategory/';
-String ADD_SUB_CATG_URL = BASE_URL + 'store/products/addSubcategory/';
+String GET_SUB_CATG_URL = BASE_URL + 'store/products/getByCategory/';
 
 //BRANDS
 String GET_BRANDS_URL = BASE_URL + 'store/products/brands/';
-String ADD_BRANDS_URL = BASE_URL + 'store/products/addBrand/';
 
 //CARS NAME
-String GET_CARS_URL = BASE_URL + 'getByBrand/';
-String ADD_CARS_URL = BASE_URL + 'store/products/addCars/';
+String GET_CARS_URL = BASE_URL + 'products/getByBrand/';
 
 //VARIANTS
 String GET_VARIANTS_URL = BASE_URL + 'getCarVarients/';
-String ADD_VARIANTS_URL = BASE_URL + 'store/products/addcarmodels/';
 
 // CAR PARTS
 String GET_ALL_CAR_PART_URL = BASE_URL + 'store/products/getCarParts/';
 String GET_CAR_PART_URL = BASE_URL + 'store/products/carParts/';
-String ADD_CAR_PART_URL = BASE_URL + 'store/products/addCarParts/';
 
 //ORDERS
-String GET_ORDERS_LIST = BASE_URL + 'store/products/getOrders/';
+String GET_ORDERS_LIST = BASE_URL + 'store/orders/getOrders/';
 String POST_ADD_ORDER = BASE_URL + 'store/products/newOrder/';
 
 // Part By Id
